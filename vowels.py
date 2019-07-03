c77f9ba745e5a98284aa2933a73bf8bd94d5b13c
Mohan <48092723+Mohan5545@users.noreply.github.com>
2019-07-03 09:31:05 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,9 @@
+set1=["a","e","i","o","u"]
+set2=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+word=str(input())
+if word in set1:
+  print("Vowel")
+elif word in set2:
+  print("Consonent")
+else:
+  print("invalid")
